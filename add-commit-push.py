cea9479d04419457035f58ca318ed8f98b30fa28
Eric <ericlrodriguez@lewisu.edu>
2023-11-06 02:57:02 -0600

    update

---
+++ add-commit-push.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+commitCommand = '\ngit commit -m "Update."'
+if len(sys.argv) == 3:
+    print(sys.argv[1])
+    if sys.argv[1] == '-m':
+        commitCommand = '\ngit commit -m "' + sys.argv[2] + '"'
+
+print(commitCommand)
+
+print('add-commit-push')
+print('\ngit status')
+os.system('git status')
+
+print('\ngit add -A')
+os.system('git add -A')
+print(commitCommand)
+os.system(commitCommand)
+print('\ngit push')
+os.system('git push')
